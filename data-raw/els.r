1590885ffca8d5ae683bd2aae406d1682108fdfd
Dana Seidel <dpseidel@berkeley.edu>
2018-12-05 13:05:52 -0800

    add package data, edit description

---
+++ data-raw/els.r
@@ -0,0 +1,22 @@
+# els.rda creation
+
+# read in pre-cleaned elephant data
+clean_els <- readr::read_csv("../Elephants/Data/clean_allels.csv", col_types = "ddTdddddddccc")
+
+# filter out 2 elephants, of different fix rates, for all of 2010
+library(dplyr)
+traj <- filter(clean_els, id %in% c("AG195", "AG268"), year(date) == 2010) %>% adehabitatLT::dl(.)
+
+
+# regularize
+els <- c(
+  adehabitatLT::setNA(traj[1], date.ref = traj[[1]]$date[1], dt = 20, units = "min"),
+  adehabitatLT::setNA(traj[2], date.ref = traj[[2]]$date[1], dt = 15, units = "min")
+) %>% ld() %>% dplyr::select(x, y, date, id) %>% mutate(id = as.character(id)) %>% split(.$id)
+
+AG195 <- els[[1]]
+AG268 <- els[[2]]
+
+# export
+usethis::use_data(AG195, overwrite = T)
+usethis::use_data(AG268, overwrite = T)
