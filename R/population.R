aa3f9d48a8983b274de950dc3f3aa0fffd7ccb1b
Dana Seidel <dpseidel@berkeley.edu>
2019-01-29 16:06:17 -0800

    add population plot functions

--- R/population.R
+++ R/population.R
@@ -1,3 +1,41 @@
-population_report <- function(list) {
+#' Spatial Temporal Distribution Plot
+#'
+#' @param df a dataframe containing columns "x", "y", "date", and "id"
+#' @param proj4 a character string indicating the proj.4 definition of the
+#' coordinate reference system defining the relocations
+#' @export
+dist_map <- function(df) {
+  mean_sf <- df %>%
+    dplyr::group_by(id) %>%
+    dplyr::summarise(
+      meanX = mean(x, na.rm = T),
+      meanY = mean(y, na.rm = T),
+      meanyear = factor(round(mean(lubridate::year(date)), 0))
+      ) %>%
+    st_as_sf(coords = c("meanX", "meanY"), remove = FALSE) %>%
+    st_set_crs(proj4)
 
+  ggplot(data = mean_sf) +
+      geom_sf(aes(fill = meanyear)) +
+      geom_text_repel(mapping = aes(
+        x = meanX, y = meanY,
+        label = id, color = meanyear
+      )) +
+      ggtitle("General Spatial and Temporal Distribution of Individuals")
+
+  return(mean_sf)
+}
+
+#' Population Timeline Plot
+#'
+#'
+#' @param df a dataframe containing columns "x", "y", "date", and "id"
+#' @export
+plot_timeline <- function(df){
+
+  df %>% group_by(id) %>%
+    summarise(start_date = min(date), end_date =  max(date)) %>%
+    ggplot() + geom_segment(aes(x = start_date, xend = end_date,
+                                y = id, yend = id, color= id),
+                            linetype=1, size=2)
 }
