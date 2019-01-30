cd93da5d274adc8dfd2ffceddf2739253dfe279c
Dana Seidel <dpseidel@berkeley.edu>
2019-01-30 11:57:47 -0800

    update pkg data

--- data-raw/els.r
+++ data-raw/els.r
@@ -6,7 +6,7 @@ library(adehabitatLT)
 clean_els <- read_csv("../Elephants/Data/clean_allels.csv", col_types = "ddTdddddddccc")
 
 # filter out 2 elephants, of different fix rates, for all of 2010
-traj <- filter(clean_els, id %in% c("AG195", "AG268"), year(date) == 2010) %>% adehabitatLT::dl(.)
+traj <- filter(clean_els, id %in% c("AG195", "AG268"), lubridate::year(date) == 2010) %>% adehabitatLT::dl(.)
 
 AG268_raw <- ld(traj[2])
 write_csv(AG268_raw, "data-raw/AG268_raw.csv")
@@ -17,13 +17,17 @@ els <- c(
   adehabitatLT::setNA(traj[2], date.ref = traj[[2]]$date[1], dt = 15, units = "min")
 ) %>% ld() %>% dplyr::select(x, y, date, id) %>% mutate(id = as.character(id)) %>% split(.$id)
 
-AG195 <- els[[1]]
-AG268 <- els[[2]]
+AG195 <- stmove::kalman(els[[1]])
+AG268 <- stmove::kalman(els[[2]])
 
 write_csv(AG195, "data-raw/AG195.csv")
-write_csv(AG195, "data-raw/AG195.csv")
+write_csv(AG268, "data-raw/AG268.csv")
+
+elephants <- rbind(AG195, AG268)
+write_csv(elephants, "data-raw/elephants.csv")
 
 # export
 usethis::use_data(AG195, overwrite = T)
 usethis::use_data(AG268, overwrite = T)
 usethis::use_data(AG268_raw, overwrite = T)
+usethis::use_data(elephants, overwrite = T)
